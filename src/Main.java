f0f9ec44e54bd2d19535846a0766a0b429a31ca7
NUWANI SUDARSHIKA <nuwanisudarshi@gmail.com>
2023-03-21 13:47:09 +0530

    constructor calling practical

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    Main(){
+
+        String name = "Programming Language is";
+        System.out.println(name+ " Java");
+    }
+    public static void main(String[] args) {
+        Main m = new Main();
+    }
+}
